9366e44eccf4dbb856b8d951e6648c3c86a80987
gbrzhezinskiy <g.brzhezinskiy@mpgames.rocks>
2021-08-12 12:57:19 +0300

    symbolic links lib

---
+++ Assets/Scripts/CreateSymbolicLink.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class CreateSymbolicLink
+{
+    [DllImport("Kernel32.dll", CharSet = CharSet.Unicode )]
+    private static extern bool CreateHardLink(string lpFileName, string lpExistingFileName, IntPtr lpSecurityAttributes);
+
+    public static void Link(string lpFileName, string lpExistingFileName)
+    {
+        CreateHardLink(lpFileName, lpExistingFileName, IntPtr.Zero);
+    }
+}
